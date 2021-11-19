0385edaf40116529ebe94ded5d926988d9063384
CodingLinhTinh <92833984+CodingLinhTinh@users.noreply.github.com>
2021-11-19 09:29:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementClickInterceptedException
 import time
+
 import InstaFollower
+
 #Your IG
 username = ''
 password = ''
@@ -15,10 +16,21 @@ acc_follower = ''
 chrome_driver_path = ''
 ser = Service(chrome_driver_path)
 driver = webdriver.Chrome(service = ser)
-                
+
+
 
 #Run the project
 ins = InstaFollower(driver)
+
+#Login
 ins.login(username,password)
-ins.find_followers(similar_acc)
-ins.follow()
+
+#follow
+def find_followers_and_follow():
+    ins.find_followers(acc_follower)
+    ins.follow()
+    
+#unfollow
+def unfollow_my_followers():
+    ins.goto_profile(username)
+    ins.unfollow()
